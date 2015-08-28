fb917c8e986b973837673da96bd2f7dd959d5be8
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-08-28 18:58:48 +0100

    Initial commit
    Includes the following features:
      * All the things you need to know about Git in a glance.
      * A subtle timestamp hanging out off to the right.
      * Previous command status.

---
+++ fish_greeting.fish
@@ -0,0 +1,12 @@
+# Set global color styles, for example:
+#
+# function default_error
+#   set_color -o red
+# end
+#
+# function default_normal
+#   set_color normal
+#
+
+function fish_greeting
+end
