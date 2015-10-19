260fe173ba2ffc3445a00d1cbb119aa290234c0b
Derek Stavis <derek@dba.eng.br>
2015-10-19 13:12:46 -0200

    Add a space between fish components when not OS X
    Freetype renders the first fish character in a weird way that the
    greater than sign (>) appears over it. Adding a space between the two
    characters solves the issue.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -7,7 +7,7 @@ set _color_error      (printf $fish_color_error ^/dev/null; or echo red --bold)
 set _color_directory  (printf $fish_color_quote ^/dev/null; or echo brown)
 set _color_repository (printf $fish_color_cwd ^/dev/null; or echo green)
 
-set _prompt_fish         "⋊>"
+set _prompt_fish         (test (uname) = Darwin; and echo "⋊>"; or echo "⋊ >")
 set _prompt_vcs_clean    "○"
 set _prompt_vcs_touched  "●"
 set _prompt_vcs_ahead    "↑"
