38a404d533f49c402f4a9212319ce70395d740d8
Stephen M. Coakley <me@stephencoakley.com>
2022-05-29 15:21:07 -0500

    Remove usage of fish pager colors (#15)
    * Remove usage of fish pager colors
    
    * Update fish_prompt.fish
    
    * Update fish_prompt.fish
    
    * Update fish_prompt.fish
    
    Co-authored-by: Pablo Aguiar <scorphus@gmail.com>

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -23,7 +23,7 @@ function fish_prompt
   set -l none     "◦"
 
   set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress 2> /dev/null; or set_color cyan)
+  set -l success_color    (set_color cyan)
   set -l error_color      (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
   set -l directory_color  (set_color $fish_color_quote 2> /dev/null; or set_color brown)
   set -l repository_color (set_color $fish_color_cwd 2> /dev/null; or set_color green)
